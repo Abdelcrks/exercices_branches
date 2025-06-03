2131d8d0d97f8dc15cf522bab232b473feab5295
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-06-03 14:58:57 +0200

    exercice terminé

---
+++ syntaxe/declaration.js
@@ -0,0 +1,13 @@
+let name = "Abdel"
+let surName = " Berkat"
+alert("Bonjour je suis " + name + surName)
+const number = 64
+let age = 26
+
+let total = number - age 
+console.log(total)
+
+alert(total + " années à travailler avant la retraite ! Courage ! En développant ce sera plus fun ! ")
+
+
+
