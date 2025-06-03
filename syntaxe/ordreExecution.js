7cc90e1789612f5f6a8063caaac8fa87bb8c53f6
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-06-03 16:01:36 +0200

    exercice terminé

---
+++ syntaxe/ordreExecution.js
@@ -0,0 +1,25 @@
+
+// firstLanguage, secondLanguage  et thirdLanguage  à la fin du code suivant :
+
+let firstLanguage = "HTML"; 
+let secondLanguage = "PHP";
+let thirdLanguage = firstLanguage ;
+firstLanguage  = "Javascript";
+secondLanguage = firstLanguage 
+
+// firstLanguage = javascript
+// secondLanguage = javascript
+// thirdLanguage = html
+
+
+// firstLanguage, secondLanguage  et thirdLanguage  à la fin du code suivant :
+
+let firstLanguage  = "HTML";
+let secondLanguage  = "PHP";
+firstLanguage  = "Javascript";
+let thirdLanguage  = firstLanguage ;
+secondLanguage  = firstLanguage 
+
+// firstLanguage = javascript
+// secondLanguage = javascript
+// thirdLanguage = javascript
