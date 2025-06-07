048e4389fdd521ca2ffb5c4fde5343ac956134a2
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-06-07 13:45:42 +0200

    Ajout du dossier Fonctions

--- syntaxe/declaration.js
+++ syntaxe/declaration.js
@@ -1,6 +1,8 @@
 let name = "Abdel"
 let surName = " Berkat"
-alert("Bonjour je suis " + name + surName)
+
+alert("Bonjour, " + name + surName)
+
 const number = 64
 let age = 26
 
@@ -8,6 +10,3 @@ let total = number - age
 console.log(total)
 
 alert(total + " années à travailler avant la retraite ! Courage ! En développant ce sera plus fun ! ")
-
-
-
