6d9bde3867eb88b704150b20fe8cf6a61db610e1
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-06-11 21:57:22 +0200

    exercice completed

---
+++ Fonctions/exercice3.js
@@ -0,0 +1,36 @@
+/* Code avant changement 
+sendMessage("Je te rends visite la semaine prochaine", "Marc", "Linda");
+
+function sendMessage(message, fromName, toName)
+{ 
+	console.log("From : " + fromName + "to : " + toName + " Message : " + message
+
+sendMessage()
+sendMessage["Super on se voit mardi !", "Linda", "Marc"]
+
+let contactName = "Jean"
+let myName = "Ada"
+let myMessage = "Je t'apprends à coder tes premières fonctions"
+sendMessage(message, fromName, toName)
+*/
+
+
+// sendMessage("Je te rends visite la semaine prochaine", "Marc", "Linda"); // ça devrait pas être avant la fonction
+
+function sendMessage(message, fromName, toName)
+{
+ console.log("From : " + fromName + "to : " + toName + " Message : " + message) 
+}// parenthèse rajouter et accolade
+
+ // sendMessage()//  inutile
+ sendMessage("Super on se voit mardi !", "Linda", "Marc") // remplacement des accolades par des parenthèses
+
+let contactName = "Jean"
+let myName = "Ada"
+let myMessage = "Je t'apprends à coder tes premières fonctions"
+   sendMessage(myMessage, myName, contactName) // utiliser les variables qui étaient declaré juste avant
+ // rajout des accolades de la fonction
+
+
+
+
