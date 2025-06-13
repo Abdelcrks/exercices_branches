94bb4e3b692c957baf883abef3d72444926ebc56
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-06-13 11:59:13 +0200

    exercice de la journée

---
+++ Fonctions/exercice10Juin.js
@@ -0,0 +1,89 @@
+
+
+
+
+/*function calcTotalPrice(articles) {
+    let total = 0
+    for (const articles of articles){
+        total += articles
+    }
+    // for(let i = 0 , i <articles.length , i++)[]
+    
+}
+*/
+/*
+const createsPrices = (articlesCount) => {
+    let prices = []
+    //console.log(1)
+    for (let i=0 ; i<articlesCount; i++){
+        let aleatoire = Math.floor(Math.random()*(10) + 1)
+       // console.log(2)
+       // let articlesAleatoires = Math.floor(Math.Random() *10) + 1
+       // prices.push(articlesCount)
+       prices.push(aleatoire)
+       //console.log(5)
+    }
+    return prices
+}
+
+console.log(createsPrices(10))
+console.log(createsPrices(5))
+console.log(createsPrices(2))
+
+*/ 
+
+
+/*
+const convertNumbertoLetter= (number) =>{
+   // console.log(number)
+    if (number === 1 ){
+        return "un"
+        }
+    else if (number === 2){
+        return "deux"
+        }
+    else if (number ===3){
+        return "trois"
+        }
+    else if (number === 4){
+        return "quatre"
+        }
+    else if (number === 5){
+        return "cinq"
+        }
+    else if (number === 6){
+        return "six"
+        }
+    else if (number === 7){
+        return "sept"
+        }
+    else if (number === 8){
+        return "huit"
+        }
+    else if (number === 9){
+        return "neuf"
+        }
+    else if (number ===10){
+        return "dix"
+        }
+
+        else 
+        return "met un chiffre entre un et dix"
+    
+}
+
+console.log(convertNumbertoLetter())
+
+
+*/ 
+
+
+
+
+const parfumParPrenoms = () => {
+    const name = ["Salem", "Vincent", "Abdel", "Meyko", "Mattéo"]
+    const parfums = ["caramel", "chocolat", "pistache", "cassis", "vanille"]
+    
+}
+
+
