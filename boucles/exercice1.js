10e3ba6fbf8084117415e841cccf6fdc76e72922
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-07-09 12:24:15 +0200

    exercice completed

---
+++ boucles/exercice1.js
@@ -0,0 +1,35 @@
+// Nous souhaitons créer des compteur.
+// Écris une boucle FOR qui affiche dans la console tous les nombres de 0 à 99.
+// Écris le même compteur avec une boucle WHILE.
+
+// Réflexion : Quel type de boucle te semble le plus approprié pour cette application ?
+// Ecris un compteur qui affiche tous les nombres de 50 à 99 (avec la boucle de ton choix).
+// Ecris un compteur qui affiche tous les nombre de 49 à 1 (avec la boucle de ton choix).
+
+let compteurAvecBoucleFor = 0
+
+for(compteurAvecBoucleFor ; compteurAvecBoucleFor<=99 ; compteurAvecBoucleFor ++)
+    console.log(compteurAvecBoucleFor)
+
+
+let compteurAvecBoucleWhile = 0
+
+while(compteurAvecBoucleWhile<=99){
+    console.log(compteurAvecBoucleWhile)
+    compteurAvecBoucleWhile +=1
+}
+
+// la boucle for semble plus approprié que while
+
+
+let compteur50A99 = 50
+
+for(compteur50A99 ; compteur50A99<= 99 ; compteur50A99 ++)
+    console.log(compteur50A99)
+
+let compteur49A1= 49
+
+for(compteur49A1 ; compteur49A1 >=1 ; compteur49A1 -- )
+    console.log(compteur49A1)
+
+
