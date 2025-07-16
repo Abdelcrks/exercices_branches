767ae1c6b461087576b1f2bfce324129551c33bd
Abdelcrks <abdelmoumenberkat@gmail.com>
2025-07-16 10:32:51 +0200

    exercise 2 completed

--- fetch-async-await/script.js
+++ fetch-async-await/script.js
@@ -1,20 +1,30 @@
+const loadingText = document.getElementById("loading")
+const divContainer =document.getElementById("container")
+
+
+
 async function fetchOffers() {
+
+    loadingText.innerText = "Chargement des offres… ⏳"
+
     const response = await fetch("https://www.codepassport.dev/api/offers")
     const offers = await response.json()
     //console.log(offers)
-
-    const div =document.getElementById("container")
+    
+    loadingText.style.display = "none"
 
     offers.forEach(element => {
         //console.log(element)
         let title = document.createElement("h2")
-        div.appendChild(title)
+        divContainer.appendChild(title)
         title.innerHTML = element.titre
+
         let technologie = document.createElement("p")
-        div.appendChild(technologie)
+        divContainer.appendChild(technologie)
         technologie.innerHTML = element.technologie
+
         let description = document.createElement("p")
-        div.appendChild(description)
+        divContainer.appendChild(description)
         description.innerHTML = element.description
         
     });
